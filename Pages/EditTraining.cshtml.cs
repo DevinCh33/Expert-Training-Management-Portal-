6b1a48639317c29c61e2ccdd648e6f1e67a74061
FungYS <fungys2020@gmail.com>
2023-04-08 22:08:42 +0800

    Adds a new page and adds a button that can link to said page.

---
+++ Pages/EditTraining.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETMP.Pages
+{
+    public class EditTrainingModel : PageModel
+    {
+        private readonly ILogger<EditTrainingModel> _logger;
+
+        public EditTrainingModel(ILogger<EditTrainingModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
