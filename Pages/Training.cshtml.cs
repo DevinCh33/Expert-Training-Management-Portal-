e2ba627bca84f463d11d08da78f41a7173819571
mmz-duckyy <114811934+mmz-duckyy@users.noreply.github.com>
2023-04-08 13:27:49 +0800

    commit7

---
+++ Pages/Training.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETMP.Pages
+{
+    public class TrainingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
