f1cc5b3faf3851c77811b622da954ffeb556e936
FungYS <fungys2020@gmail.com>
2023-04-08 23:06:29 +0800

    Adds buttons for the training editor for both admins and users

---
+++ Pages/UserEditTraining.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETMP.Pages
+{
+    public class UserEditTrainingModel : PageModel
+    {
+        private readonly ILogger<UserEditTrainingModel> _logger;
+
+        public UserEditTrainingModel(ILogger<UserEditTrainingModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
