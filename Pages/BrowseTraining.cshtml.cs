7617c6ca0634e7098e199b51557ec527ab3d8ad1
EitonNg <76936894+EitonNg@users.noreply.github.com>
2023-04-08 21:59:12 +0800

    Changed Training Page to BrowseTraining !!Gabriel

---
+++ Pages/BrowseTraining.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ETMP.Model;
+using ETMP.Data;
+
+namespace ETMP.Pages
+{
+    public class BrowseTrainingModel : PageModel
+    {
+        public List<TrainingModel> Trainings = new List<TrainingModel>();
+
+        private readonly ILogger<BrowseTrainingModel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public BrowseTrainingModel(ILogger<BrowseTrainingModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public void OnGet()
+        {
+            Trainings = _context.Trainings.ToList();
+        }
+    }
+}
