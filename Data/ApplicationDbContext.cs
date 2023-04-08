8d64978935dadc4501772826070c347f4b3858ce
EitonNg <76936894+EitonNg@users.noreply.github.com>
2023-04-08 21:31:49 +0800

    Added Database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ETMP.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ETMP.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<TrainingModel> Trainings { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
