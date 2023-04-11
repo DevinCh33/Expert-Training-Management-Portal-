51780618b498c1b86cb9ae9fc8fb2e86d465b173
DevinCh33 <102984883+DevinCh33@users.noreply.github.com>
2023-04-11 23:25:32 +0800

    merge
    update to pull

---
+++ Pages/edituserprofile.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;o
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETMP.Pages
+{
+    public class Index1Model : PageModel
+    {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
