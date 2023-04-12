f6a4e62aaf59138d02ba907423c3b2f8e93747f7
FungYS <fungys2020@gmail.com>
2023-04-12 21:47:27 +0800

    js bad

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,6 +4,10 @@
 // Write your JavaScript code.
 var sortBtn = document.querySelector("#sort-btn");
 var dropdownContent = document.querySelector(".dropdown-content");
+var priceRange = document.getElementById("price-range");
+var priceDisplay = document.getElementById("price-display");
+
+
 sortBtn.addEventListener("mouseenter", function () {
     dropdownContent.classList.add("active");
 });
@@ -16,8 +20,7 @@ dropdownContent.addEventListener("mouseleave", function () {
     dropdownContent.classList.remove("active");
 });
 
-var priceRange = document.getElementById("price-range");
-var priceDisplay = document.getElementById("price-display");
+
 priceRange.addEventListener("input", function () {
     priceDisplay.innerHTML = "RM" + priceRange.value;
 });
@@ -28,6 +31,51 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#applyFilterBtn").click(function () {
+        $(".filterInterface").toggleClass("visible");
+    });
+});
+
+
+var slideshowContainer = document.querySelector(".slideshowContainer");
+var imageContainers = document.querySelectorAll(".imageContainer");
+var arrows = document.querySelectorAll(".arrow");
+var dots = document.querySelectorAll(".dot");
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+arrows.forEach(arrow => {
+    arrow.addEventListener("click", () => {
+        if (arrow.classList.contains("prev")) {
+            showSlides(slideIndex -= 1);
+        } else {
+            showSlides(slideIndex += 1);
+        }
+    });
+});
+
+// Thumbnail image controls
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        showSlides(slideIndex = index + 1);
+    });
+});
+
+function showSlides(n) {
+    if (n > imageContainers.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = imageContainers.length;
+    }
+    imageContainers.forEach(container => container.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+    imageContainers[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
+}
 var dropdown = document.getElementById("training-options");
 var addForm = document.getElementById("add-training");
 
