01aa252f9ac39328cad7383ab3e5d5016c0a5df3
mmz-duckyy <114811934+mmz-duckyy@users.noreply.github.com>
2023-04-19 22:43:18 +0800

    Added Table, refactor code

--- Models/TrainingModel.cs
+++ Models/TrainingModel.cs
@@ -14,38 +14,8 @@ namespace ETMP.Models
         public bool Availability { get; set; }
         public string? TrainingDescription { get; set; }
 
-        public List<TrainingModel> GetTrainingData()
-        {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-ETMP-53bc9b9d-9d6a-45d4-8429-2a2761773502;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-
-            string sqlQuerry = "SELECT * FROM [Identity].[Trainings]";
-
-            SqlCommand cmd = new SqlCommand(sqlQuerry, con);
-
-            SqlDataReader dr = cmd.ExecuteReader();
-
-            List<TrainingModel> trainingList = new List<TrainingModel>();
-            while (dr.Read())
-            {
-                TrainingModel trainingModel = new TrainingModel();
-                trainingModel.Id = int.Parse((dr["Id"].ToString()));
-                trainingModel.TrainingName = dr["TrainingName"].ToString();
-                trainingModel.TrainingPrice = int.Parse((dr["TrainingPrice"].ToString()));
-                trainingModel.TrainingItinerary = dr["TrainingItinerary"].ToString();
-                trainingModel.TrainingVenue = dr["TrainingVenue"].ToString();
-                trainingModel.TrainingCategory = dr["TrainingCategory"].ToString();
-                trainingModel.Availability = bool.Parse(dr["Availability"].ToString());
-                trainingModel.TrainingDescription = dr["TrainingDescription"].ToString();
-                trainingList.Add(trainingModel);
-            }
-            con.Close();
-            return trainingList;
-        }
-
-
     }
+
 }
 
 
--- Pages/BrowseTraining.cshtml.cs
+++ Pages/BrowseTraining.cshtml.cs
@@ -1,15 +1,55 @@
+using ETMP.Data;
 using ETMP.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Data.SqlClient;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETMP.Pages
 {
-    public class BrowseTrainingModel : PageModel
+    public class BrowseTraining : PageModel
     {
-        public List<TrainingModel> trainings { get; set; }
+        public List<TrainingModel> Trainings { get; set; }
+        private readonly ApplicationDbContext _context;
 
-        
+        public BrowseTraining(ApplicationDbContext context)
+        {
+            
+            _context = context;
+            
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Trainings = await _context.Trainings.ToListAsync();
+            return Page();
+        }
+        /*
+        public async Task<IActionResult> OnGetTrainingAsync(string sortColumn = "TrainingName", string sortOrder = "asc")
+        {
+            var trainingsQuery = _context.Trainings.AsQueryable();
+
+            switch (sortColumn.ToLower())
+            {
+                case "trainingname":
+                    trainingsQuery = sortOrder.ToLower() == "desc"
+                        ? trainingsQuery.OrderByDescending(t => t.TrainingName)
+                        : trainingsQuery.OrderBy(t => t.TrainingName);
+                    break;
+                case "trainingprice":
+                    trainingsQuery = sortOrder.ToLower() == "desc"
+                        ? trainingsQuery.OrderByDescending(t => t.TrainingPrice)
+                        : trainingsQuery.OrderBy(t => t.TrainingPrice);
+                    break;
+                    // add more cases for other sortable columns as needed
+            }
+
+            Trainings = await trainingsQuery.ToListAsync();
+
+            return Page();
+        }*/
+
+
     }
 }
+
 
