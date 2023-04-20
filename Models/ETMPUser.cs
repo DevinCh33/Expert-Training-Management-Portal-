7cde60106f065d9053e256447073270f3d3c3404
FungYS <fungys2020@gmail.com>
2023-04-20 21:35:16 +0800

    test

--- Models/ETMPUser.cs
+++ Models/ETMPUser.cs
@@ -10,6 +10,5 @@ namespace ETMP.Models
         public string ? Gender { get; set; }
         public string ? FirstName { get; set; }
         public string ? LastName { get; set; }
-        public List<TrainingModel>? PurchasedTraining { get; set; } = new List<TrainingModel>();
     }
 }
