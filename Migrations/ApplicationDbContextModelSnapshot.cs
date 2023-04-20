9333d4459f90d2741bfc8c293aff2f780c28c2a3
DevinCh33 <102984883+DevinCh33@users.noreply.github.com>
2023-04-20 22:23:48 +0800

    deleted

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -117,14 +117,13 @@ namespace ETMP.Migrations
                     b.Property<bool>("Availability")
                         .HasColumnType("bit");
 
-                    b.Property<string>("ETMPUserId")
-                        .HasColumnType("nvarchar(450)");
-
                     b.Property<string>("TrainingCategory")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("TrainingDescription")
+                        .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("TrainingImgURL")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("TrainingItinerary")
@@ -140,8 +139,6 @@ namespace ETMP.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("ETMPUserId");
 
                     b.ToTable("Trainings", "Identity");
                 });
@@ -279,13 +276,6 @@ namespace ETMP.Migrations
                     b.ToTable("UserTokens", "Identity");
                 });
 
-            modelBuilder.Entity("ETMP.Models.TrainingModel", b =>
-                {
-                    b.HasOne("ETMP.Models.ETMPUser", null)
-                        .WithMany("PurchasedTraining")
-                        .HasForeignKey("ETMPUserId");
-                });
-
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
                 {
                     b.HasOne("Microsoft.AspNetCore.Identity.IdentityRole", null)
@@ -335,11 +325,6 @@ namespace ETMP.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
-                });
-
-            modelBuilder.Entity("ETMP.Models.ETMPUser", b =>
-                {
-                    b.Navigation("PurchasedTraining");
                 });
 #pragma warning restore 612, 618
         }
