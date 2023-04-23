941cf4c08aa71812494feadb9d972d6dfadcaa88
mmz-duckyy <114811934+mmz-duckyy@users.noreply.github.com>
2023-04-23 22:40:33 +0800

    added search bar functions, fixed filter bugs, added a new attribute to trainingmodel.cs

--- Migrations/20230423115452_dcvbgfdf.cs
+++ Migrations/20230423115452_dcvbgfdf.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ETMP.Migrations
 {
     /// <inheritdoc />
-    public partial class pleaseWork : Migration
+    public partial class dcvbgfdf : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
--- Migrations/20230423115939_WETG.Designer.cs
+++ Migrations/20230423115939_WETG.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ETMP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230422165414_pleaseWork")]
-    partial class pleaseWork
+    [Migration("20230423115939_WETG")]
+    partial class WETG
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
