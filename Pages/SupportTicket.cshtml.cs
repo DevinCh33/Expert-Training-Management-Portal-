cb2b1078b389ac498c4aaef297abcc6b5332b949
FungYS <fungys2020@gmail.com>
2023-05-05 22:46:28 +0800

    link to support ticket

---
+++ Pages/SupportTicket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETMP.Pages
+{
+    public class SupportTicketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
