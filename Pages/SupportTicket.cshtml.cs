0c0cf5e91d5af0f53bdb83bc55c6cb1f887dd16b
FungYS <fungys2020@gmail.com>
2023-05-08 21:50:40 +0800

    form validation

--- Pages/SupportTicket.cshtml.cs
+++ Pages/SupportTicket.cshtml.cs
@@ -1,41 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.ComponentModel.DataAnnotations;
 namespace ETMP.Pages
 {
     public class SupportTicketModel : PageModel
     {
-        private string _name;
-        private string _email;
-        private string _about;
-        private string _description;
+        private string? _name;
+        private string? _email;
+        private string? _about;
+        private string? _description;
 
+        [BindProperty]
+        [Required(ErrorMessage = "Name is required")]
         public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
 
-        private string Email
+        [BindProperty]
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
+        public string Email
         {
             get { return _email; }
             set { _email = value; }
         }
 
-        private string About
+        [BindProperty]
+        [Required(ErrorMessage = "This field must not be empty")]
+        public string About
         {
             get { return _about; }
             set { _about = value; }
         }
 
-        private string Description
+
+        [BindProperty]
+        [Required(ErrorMessage = "This field must not be empty")]
+        public string Description
         {
             get { return _description; }
             set { _description = value; }
         }
-
-        public void OnGet()
+        public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("UserListOfTraining");
+            }
         }
     }
 }
