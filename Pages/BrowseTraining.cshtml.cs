906f8bc011a41cc9ee0d4def32f78e4c421d9389
mmz-duckyy <114811934+mmz-duckyy@users.noreply.github.com>
2023-05-11 23:49:26 +0800

    x

--- Pages/BrowseTraining.cshtml.cs
+++ Pages/BrowseTraining.cshtml.cs
@@ -12,6 +12,7 @@ using System.Text;
 using ETMP.Services;
 using ETMP.Hubs;
 using System.Configuration;
+using System.Web.Helpers;
 
 namespace ETMP.Pages
 {
@@ -21,20 +22,33 @@ namespace ETMP.Pages
 
         public List<TrainingModel> Trainings { get; set; }
         private readonly ApplicationDbContext _context;
-
-
-
-        public BrowseTraining(ApplicationDbContext context)
+        //newly added
+        private readonly Services.IMailService _mailService;
+        private readonly UserManager<ETMPUser> _userManager;
+        private readonly SignInManager<ETMPUser> _signInManager;
+        public string data { get; set; }
+        public string userdata { get; set; }
+        private List<TrainingModel> _trainingModels;
+        public BrowseTraining(ApplicationDbContext context, Services.IMailService mailService, UserManager<ETMPUser> userManager, SignInManager<ETMPUser> signInManager)
         {
             _context = context;
-
-            
+            //newly added
+            _mailService = mailService;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
 
         public async Task<IActionResult> OnGetAsync()
         {
             Trainings = await _context.Trainings.ToListAsync();
+            //newly added
+            string subject = "Support Ticket On xd";
+            string body = "Name of Sender: <br/><br/>Email of Sender: <br/><br/>Description of Issue: ";
+            string toEmail = "emtpsdnbhd@gmail.com";
+            var request = new MailRequest(toEmail, subject, body, null);
+            await _mailService.SendEmailAsync(request);
+
             return Page();
         }
 
