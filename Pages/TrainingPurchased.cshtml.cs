544979a8ba8d7c25b1748157a8aa96b03da143f1
mmz-duckyy <114811934+mmz-duckyy@users.noreply.github.com>
2023-05-13 23:02:35 +0800

    code34

--- Pages/TrainingPurchased.cshtml.cs
+++ Pages/TrainingPurchased.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Org.BouncyCastle.Asn1.X509;
+using PuppeteerSharp;
 
 namespace ETMP.Pages
 {
@@ -47,8 +49,9 @@ namespace ETMP.Pages
                 if (user.PurchasedTraining != null)
                 {
                     string userExistingTraining = user.PurchasedTraining;
-
+                    
                     _trainingModels = JsonConvert.DeserializeObject<List<TrainingModel>>(userExistingTraining);
+
                     //notification
                     notification = new Notification();
                     notification.ToUserId = user.Id;
@@ -69,12 +72,35 @@ namespace ETMP.Pages
  
                     user.PurchasedTraining = JsonConvert.SerializeObject(_trainingModels);
                     userdata = user.PurchasedTraining;
+                    //
+                    
+
+                    foreach (TrainingModel training in _trainingModels)
+                    {
+                        //if training is starting within 7 days
+                        if (training.TrainingStartDateTime >= DateTime.Today &&
+                            training.TrainingStartDateTime <= DateTime.Today.AddDays(7))
+                        {
+                            Notification noti = new Notification();
+                            noti.ToUserId = user.Id;
+                            noti.ToUserName = user.UserName;
+                            noti.NotificationHeader = "Your training is coming soon!";
+                            noti.NotificationBody = "Training " + training.TrainingName + " is coming soon! The date for the training is " + training.TrainingStartDateTime;
+                            noti.NotificationDate = DateTime.Now;
+                            _context.Notification.Add(noti);
+
+                            string subj = noti.NotificationHeader;
+                            string Body = noti.NotificationBody;
+                            string toUserEmail = user.Email;
+                            var mailRequest = new MailRequest(toUserEmail, subj, Body, null);
+                            await _mailService.SendEmailAsync(mailRequest);
+                        }
+                    }
                     await _userManager.UpdateAsync(user);
                     await _signInManager.RefreshSignInAsync(user);
                 }
                 else
                 {
-   
                     user.PurchasedTraining = "[" + JsonConvert.SerializeObject(PurchasedTraining) + "]";                    
                     await _userManager.UpdateAsync(user);
                     await _signInManager.RefreshSignInAsync(user);
